4fb7de3bac2253b6c512625b81eee37d731c0021
KavanKake <Kavin_lokeswaran1407@outlook.com>
2025-03-31 11:22:26 +0200

    Not logged in page

--- my-app/src/routes/notLoggedIn/+page.svelte
+++ my-app/src/routes/notLoggedIn/+page.svelte
@@ -1,1 +1,92 @@
-not logged in
+<script>
+    import FloatingDots from "../../lib/komponent/FloatingDots.svelte";
+    let showAnimation = true;
+</script>
+
+
+<div class="page">
+    <div class="container">
+        <div class="upperpart">
+            <h1>You are not logged in</h1>
+            <h1>Log in and get back to work!</h1>
+        </div>
+        <div class="lowerpart">
+            <a class="login-button" href="/login">Log in</a>
+        </div>
+    </div>
+</div>
+
+
+
+{#if showAnimation}
+  <FloatingDots />
+{/if}
+
+<style>
+    .page{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+    .container{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 2em;
+        height: 30em;
+        width: 40em;
+        border: #3B0086 2px solid;
+        border-radius: 20px 20px 20px 20px;
+        background-color: #0D5C63;
+        opacity: 0.9;
+        font-family: coolvetica;
+    }
+
+    .upperpart{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 1em;
+    }
+
+    .lowerpart{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: center;
+        align-items: center;
+        gap: 1em;
+    }
+
+    .login-button{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        color: white;
+        font-family: coolvetica;
+        font-size: 1.5em;
+        background-color: #0D5C63;
+        border: 2px solid #000000;
+        border-radius: 20px 20px 20px 20px;
+        width: 20em;
+        height: 3em;
+        transition: all 0.5s ease-in-out;
+        &&:hover{
+            scale: 1.2;
+        }
+    }
+
+    h1{
+        color: white;
+        margin: 0;
+    }
+
+    a{
+        text-decoration: none;
+    }
+</style>
