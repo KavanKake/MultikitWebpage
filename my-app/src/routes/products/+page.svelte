f4d9bedb3d2fcf0ececec5c5817a2677a5c22690
KavanKake <Kavin_lokeswaran1407@outlook.com>
2025-06-12 09:08:25 +0200

    Products siden

---
+++ my-app/src/routes/products/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { goto } from '$app/navigation';
+    import { onMount } from 'svelte';
+  
+    // Redirect after mount
+    onMount(() => {
+      goto('/error404');
+    });
+  </script>
